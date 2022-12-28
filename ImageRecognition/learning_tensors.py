2b81e98afb0a2765ca9e23e0a3b0296c55d4be53
Jonathan Baker <35644651+SnicketySmock@users.noreply.github.com>
2022-12-28 15:42:39 -0800

    completed learning_tensors.py

--- ImageRecognition/learning_tensors.py
+++ ImageRecognition/learning_tensors.py
@@ -1,5 +1,7 @@
 import torch
 import numpy as np
+import time
+start = time.time()
 
 data = [[1, 2], [3, 4]]
 x_data = torch.tensor(data)
@@ -40,3 +42,30 @@ print(t1)
 
 print(f"tensor.mul(tensor) \n {tensor.mul(tensor)} \n")
 print(f"tensor * tensor) \n {tensor * tensor} \n")
+
+print(f"tensor.matmul(tensor.T) \n {tensor.matmul(tensor.T)} \n")
+print(f"tensor @ tensor.T) \n {tensor @ tensor.T} \n")
+
+print(tensor, "\n")
+tensor.add_(5)
+print(tensor)
+
+t = torch.ones(5)
+print(f"t: {t}")
+n = t.numpy()
+print(f"n: {n}")
+
+t.add_(1)
+print(f"t: {t}")
+print(f"n: {n}")
+
+n = np.ones(5)
+t = torch.from_numpy(n)
+
+np.add(n, 1, out=n)
+print(f"t: {t}")
+print(f"n: {n}")
+
+end = time.time()
+total_time = end - start
+print(f"Exec Time: {str(total_time)} sec")
