24ae892359a3f71d7c4f53f34f2af382f9c36407
Jonathan Baker <35644651+SnicketySmock@users.noreply.github.com>
2022-12-29 10:28:16 -0800

    Finished learning_autograd.py

--- ImageRecognition/learning_autograd.py
+++ ImageRecognition/learning_autograd.py
@@ -12,3 +12,14 @@ loss.backward()  # backward pass
 optim = torch.optim.SGD(model.parameters(), lr=1e-2, momentum=0.9)
 
 optim.step()  # gradient descent
+
+a = torch.tensor([2., 3.], requires_grad=True)
+b = torch.tensor([6., 4.], requires_grad=True)
+
+Q = 3*a**3 - b**2
+
+external_grad = torch.tensor([1., 1.])
+Q.backward(gradient=external_grad)
+
+print(9*a**2 == a.grad)
+print(-2*b == b.grad)
